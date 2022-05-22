265f89de88b73db49acba6abd1bed1d04dc878a5
Zak Vanstrom <zvanstro@asu.edu>
2022-05-21 17:04:54 -0700

    fantastic progress. no more failure
    added: results reporter
    added: performance testing (exportable to other projects)

--- canvas-to-quizlet.py
+++ canvas-to-quizlet.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup as bs
 import re
 import time
 
+q_seen = 0
+a_seen = 0
+q_written = 0
+a_written = 0
+failure = 0
+
 
 def performance_test(func, args, repetitions=10, arg_list=None, name=None, print_times=True):
     def now(): return time.process_time()
@@ -46,21 +52,35 @@ def performance_test(func, args, repetitions=10, arg_list=None, name=None, print
 
 
 def get_pairs(file, log=False) -> dict:
-    scrape = bs(file)
+    global a_seen, q_seen
+    scrape = bs(file, features="html.parser")
     all_questions = scrape.select('div.text')
     pairs = []
     if log:
         print("Question Count: " + str(len(all_questions)))
     for q in all_questions:
+        q_seen += 1
         question_text = clean_up(
             q.find('div', class_='question_text user_content').text)
-        answers = q.findAll('div', class_=["correct_answer"])
-        if log:
-            print("# Answers: " + str(len(answers)))
+        answers = q.findAll('div', class_=["answer_for_"])
         answer_texts = []
         for a in answers:
-            answer_texts.append(clean_up(a['title'], 'answer'))
+            answer_string = str(a['title'])
+            if "correct answer" not in answer_string:
+                continue
+            a_seen += 1
+            cor_ans = ". The correct answer was "
+            i = answer_string.find(cor_ans)
+            if i >= 0:
+                i1 = answer_string.find(" You selected ")
+                part1 = clean_up(answer_string[0:i1])
+                part2 = clean_up(answer_string[i+len(cor_ans)::])
+                answer_string = part1 + " --> " + part2
+            answer_texts.append(clean_up(answer_string, 'answer'))
         pairs.append([question_text, answer_texts])
+        if log:
+            print("# Answers: " + str(len(answers)))
+            # print('\n\t'.join(answer_texts))
     return pairs
 
 # get pairs with Select
@@ -69,20 +89,23 @@ def get_pairs(file, log=False) -> dict:
 
 
 def clean_up(txt, object_type=''):
-    txt = str(txt).strip().replace('\n', ' ').replace(
+    txt = str(txt).replace('\n', '. ').replace(
         ';', '.').replace('\t', ' ')
     if object_type == 'answer':
         txt = txt.replace(". This was the correct answer.", '')
         txt = txt.replace(". You selected this answer", '')
         txt = txt.replace(". You selected ", " --> ")
-    return txt
+    return txt.strip().strip('.')
 
 
 def write_pairs(pairs: dict, location: str):
+    global a_written, q_written
     with open(location, 'a+', encoding="utf8") as f:
         for pair in pairs:
-            answer_as_text = '\n\n'.join(pair[1])
-            f.write(f"{pair[0]}\t{answer_as_text};\n")
+            a_written += len(pair[1])
+            q_written += 1
+            answer_as_text = '\n'.join(pair[1])
+            f.write(f"{pair[0]}\t{answer_as_text}\n\n")
 
 
 def write_html_file(file, path):
@@ -104,31 +127,60 @@ def get_HTML_paths_from_directory(dir_path):
 
 def main():
     # html_paths = [input("Enter the Input File Path")]
-    # html_paths = ["/Users/zak/dev/canvas-to-quizlet/examples/example_test_1.html"]
+    # html_paths = ["/Users/zak/dev/canvas-to-quizlet/examples/original_html/Module 4: Quiz: For all, there exists type proofs: MAT 243: Discrete Math Structures (2019 Fall) - o.html"]
     html_paths = get_HTML_paths_from_directory(
         "/Users/zak/dev/canvas-to-quizlet/examples/original_html")
 
-    output_path = get_output_path(
-        "/Users/zak/dev/canvas-to-quizlet/examples/full-size-output.txt")
+    print("File Count: ", len(html_paths))
+
+    path = "/Users/zak/dev/canvas-to-quizlet/examples/full-size-output.txt"
+    if os.path.exists(path):
+        os.remove(path)
 
     # performance_test(do_run, [html_paths, output_path], repetitions=100, print_times=False)
-    do_run(html_paths, output_path, log=True)
+    do_run(html_paths, output_path=path, log=True)
+
+    view_run_results()
 
 
-def do_run(html_paths, output_path, log=False):
-    for f in html_paths:
+def do_run(html_paths, output_path=None, log=False):
+    global failure
+    gen_paths = True
+    if output_path:
+        gen_paths = False
+    for i, f in enumerate(html_paths):
+        if log:
+            print("\n\n", f[55:100])
         file = open(f, "r", encoding="utf8")
         content = file.read()
         file.close()
         pairs = get_pairs(content, log=log)
-        if len(pairs) == 0:
+        if not pairs:
+            failure += 1
             if log:
                 print("Write Failed!")
-            return
-        output_path = get_output_path(f)
+            continue
+        if gen_paths:
+            output_path = get_output_path(f)
         write_pairs(pairs, output_path)
         if log:
-            print('\n\n', f, f'\nWrote to {output_path}.')
+            print(f'File {i+1} : Wrote to {output_path}.')
+
+
+def view_run_results():
+    print("\n\nRun Results:")
+    print("Questions Seen:", q_seen)
+    print("Questions Written", q_written)
+    if q_seen != q_written:
+        print("DISCREPANCY FOUND with QUESTIONS! A DIFFERENCE OF",
+              abs(q_seen, q_written))
+
+    print("Answers Seen:", a_seen)
+    print("Answers Written", a_written)
+    if a_seen != a_written:
+        print("DISCREPANCY FOUND with ANSWERS! A DIFFERENCE OF",
+              abs(a_seen-a_written))
+    print("Failures:", failure)
 
 
 if __name__ == "__main__":
